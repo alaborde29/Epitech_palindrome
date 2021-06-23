11ba4fad28605ee631ba57a536a11f5edc00bd35
LABORDE Alexandre <alaborde@localhost.localdomain>
2021-06-23 09:34:34 +0200

    undo commit

--- src/error_handling.c
+++ src/error_handling.c
@@ -63,20 +63,6 @@ int facult_arg_errors(int ac, char **av)
     return (0);
 }
 
-int handle_all_b_palindrome(int ac, char **av)
-{
-    int i = 0;
-
-    while (i != ac)
-    {
-        if (my_strcmp(av[i], "-b") == 0 && error_b(ac, av, i) == 0)
-            return (my_getnbr(av[i + 1]));
-        i++;
-    }
-    return (10);
-}
-// penser a verifier le palindrome dans la base adequate.
-
 int error_handling(int ac, char **av)
 {
     if (ac < 3)
@@ -87,7 +73,7 @@ int error_handling(int ac, char **av)
     || my_str_isnum(av[2]) == 0)
         return (-1);
     if (my_strcmp(av[1], "-p") == 0 && \
-    is_nb_palindrome(my_getnbr(av[2]), handle_all_b_palindrome(ac, av)) == -1)
+    is_nb_palindrome(my_getnbr(av[2]), 10) == -1)
         return (-1);
     if (ac > 3) {
         if (facult_arg_errors(ac, av) == -1)
